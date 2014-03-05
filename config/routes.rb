7c39a382839bca52102edbfacec8fddfddd34617
Yulin Dong <yulin.dong@hughes.com>
2014-03-05 15:22:57 -0700

    Added team and club and made JSON compatible only

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 League::Application.routes.draw do
+  resources :teams
+
+  resources :clubs, :except => [:new, :edit]
+  resources :teams, :except => [:new, :edit]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
